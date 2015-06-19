dcd7374635ae568f98c95c7be983d132664cd4d9
Dominik Steffen <dominik.steffen@googlemail.com>
2015-06-19 11:26:44 +0200

    Started to implement the different polls for the controller.

---
+++ src/Engine/Imp/Input/SharpDX/SharpDX/InputDeviceImp.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using SharpDX.XInput;
+
+namespace Fusee.Engine
+{
+
+    /// <summary>
+    /// The SlimDX - specific implementation for the input devices.
+    /// </summary>
+    public class InputDeviceImp : IInputDeviceImp
+    {
+        private readonly Controller _controller;
+        
+        // Settings
+        private float _deadZone = 0.1f;
+        private float _vibration = 0f;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="InputDeviceImp"/> class.
+        /// </summary>
+        /// <param name="instance">The DeviceInstance.</param>
+        public InputDeviceImp(Controller device)
+        {
+            _controller = device;
+            _deadZone = 0.1f;
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="InputDeviceImp"/> class.
+        /// </summary>
+        public InputDeviceImp()
+        {
+
+        }
+
+        /// <summary>
+        /// Gets the current state of the input device. The state is used to poll the device.
+        /// </summary>
+        /// <returns>The state of the input device.</returns>
+        public State GetState()
+        {
+            return _controller.GetState();
+        }
+
+        /// <summary>
+        /// Loop though all buttons on the gamepad an see which one is pressed
+        /// </summary>
+        /// <returns>The pressed button</returns>
+        public int GetPressedButton()
+        {
+            return 0;
+        }
+
+        /// <summary>
+        /// Checks if the button is down.
+        /// </summary>
+        /// <param name="buttonIndex">The button to check.</param>
+        /// <returns>True if the button is pressed and false if not.</returns>
+        public bool IsButtonDown(int buttonIndex)
+        {
+            return false;
+        }
+
+        /// <summary>
+        /// Checks if a specified button is held down for more than one frame.
+        /// </summary>
+        /// <param name="buttonIndex">The index of the button that is checked.</param>
+        /// <returns>true if the button at the specified index is held down for more than one frame and false if not.</returns>
+        public bool IsButtonPressed(int buttonIndex)
+        {
+            return false;
+        }
+
+        /// <summary>
+        /// Counts the buttons on the input device.
+        /// </summary>
+        /// <returns>The amount of buttons on the device.</returns>
+        public int GetButtonCount()
+        {
+            return 0;
+        }
+
+        /// <summary>
+        /// Gets the value of the z-axis. On most gamepads the z-axis is either the right joystick or the triggers at the back.
+        /// For wobbly joysticks a dead zone is required to avoid value changes if the joystick is not moved.
+        /// </summary>
+        /// <returns>The current value of the z-axis.</returns>
+        public float GetZAxis()
+        {
+            return 0;
+        }
+
+        /// <summary>
+        /// Gets the value of the y-axis. On most gamepads the y-axis is the horizontal axis of the left joystick.
+        /// For wobbly joysticks a dead zone is required to avoid value changes if the joystick is not moved.
+        /// </summary>
+        /// <returns>The current value of the y-axis.</returns>
+        public float GetYAxis()
+        {
+            return 0;
+        }
+
+        /// <summary>
+        /// Gets the value of the x-axis. On most gamepads the x-axis the horizontal axis of the left joystick.
+        /// For wobbly joysticks a dead zone is required to avoid value changes if the joystick is not moved.
+        /// </summary>
+        /// <returns>The current value of the x-axis.</returns>
+        public float GetXAxis()
+        {
+
+            return 0;
+        }
+
+        /// <summary>
+        /// Gets the category.
+        /// </summary>
+        /// <returns>The category of the device</returns>
+        public String GetCategory()
+        {
+            return _controller.GetCapabilities(DeviceQueryType.Any).ToString();
+        }
+
+        /// <summary>
+        /// Gets a user-friendly product name of the device.
+        /// </summary>
+        /// <returns>The device name.</returns>
+        public string GetName()
+        {
+            return _controller.GetCapabilities(DeviceQueryType.Any).ToString();
+        }
+
+        /// <summary>
+        /// Sets the Deadzone
+        /// </summary>
+        /// <param name="zone">The Deadzone.</param>
+        public void SetDeadZone(float zone)
+        {
+            _deadZone = zone;
+        }
+
+        /// <summary>
+        /// Sets the rumble motors vibration.
+        /// </summary>
+        /// <param name="zone">The rumble vibration.</param>
+        public void SetVibration(float vib)
+        {
+            _vibration = vib;
+        }
+
+
+    }
+}
+
---
+++ src/Engine/Imp/Input/SharpDX/SharpDX/InputDriverImp.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using SharpDX.XInput;
+
+namespace Fusee.Engine
+{
+    /// <summary>
+    /// Sharp DX (Microsoft XInput) specific implementation for the <see cref="IInputDriverImp"/>.
+    /// </summary>
+    class InputDriverImp : IInputDriverImp
+    {
+        public List<Controller> Devices = new List<Controller>();
+
+        /// <summary>
+        /// All SharpDX (Microsoft XInput) compatible input devices are initialised and added to a List of the type <see cref="IInputDeviceImp"./>
+        /// </summary>
+        /// <returns>A list containing all XInput compatible input devices.</returns>
+        public List<IInputDeviceImp> DeviceImps()
+        {
+            var val = UserIndex.GetValues(typeof(UserIndex));
+
+            // Loop over the enum and check for every user id.
+            foreach (UserIndex userid in val)
+            {
+                if(userid == UserIndex.Any)
+                    continue;
+
+                Devices.Add(new Controller(userid));
+            }
+            
+            var retList = new List<IInputDeviceImp>();
+            foreach (Controller device in Devices)
+            {
+                retList.Add(new InputDeviceImp(device));
+            }
+            return retList;
+        }
+    }
+}
