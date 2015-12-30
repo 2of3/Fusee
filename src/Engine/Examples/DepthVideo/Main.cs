426b4e1af2ce7a26a731f458460768d78dda71aa
Linda Schey <lindaschey@t-online.de>
2015-12-30 15:05:58 +0100

    new test videos, draing deoth value as color on objekt

--- src/Engine/Examples/DepthVideo/Main.cs
+++ src/Engine/Examples/DepthVideo/Main.cs
@@ -40,7 +40,7 @@ namespace Examples.DepthVideo
             void main(){
     
                 vUV = fuUV;
-                gl_Position = (FUSEE_MVP * vec4(fuVertex, 1.0))- vec4(0,0,10,1);
+                gl_Position = FUSEE_MVP * vec4(fuVertex, 1.0);
 
                 vNormal = mat3(FUSEE_ITMV[0].xyz, FUSEE_ITMV[1].xyz, FUSEE_ITMV[2].xyz) * fuNormal;
             }";
@@ -53,26 +53,74 @@ namespace Examples.DepthVideo
             uniform sampler2D  textureDepth;
             varying vec3 vNormal;
             varying vec2 vUV;
+            varying vec3 fuVertex;
             uniform mat4 FUSEE_MV;
- 
-            float zvalue(float posz)
-            {
-                return ((10+1)/(10-1))+(1/posz)*((-2*10*1)/(10-1));
-            }
+
             void main(){
                
+                vec4 pos = FUSEE_MV * vec4(fuVertex, 1.0);
                    
-                   float  myvalue = zvalue(vec3(FUSEE_MV[3].xyz).z);
-                   gl_FragColor = vec4(myvalue, myvalue, myvalue, 1);
-                   gl_FragDepth =(1-(texture(textureDepth, vUV)));
+                // float  myvalue = zvalue(pos.z);
+   
+                float depthTexValue = ((texture(textureDepth, vUV)));
+                
+                float DEPTH;
+                float fragCorrdZ = gl_FragCoord.z;
+                float div = abs(fragCorrdZ-depthTexValue);
+                DEPTH = fragCorrdZ+div;
+                gl_FragColor =texture2D(textureDepth, vUV);
+                gl_FragDepth = depthTexValue;
+                
 
            
             }";
 
 
+        private const string VsDrawDepth = @"
+            #ifdef GL_ES
+                precision mediump float;
+            #endif
+
+            attribute vec3 fuVertex;
+            attribute vec3 fuNormal;
+            attribute vec2 fuUV;
+
+            varying vec3 vNormal;
+            varying vec2 vUV;
+
+            
+            uniform mat4 FUSEE_MVP;
+            uniform mat4 FUSEE_ITMV;
+            uniform mat4 FUSEE_V;
+            uniform mat4 FUSEE_P;
+
+          
+
+            void main(){
+    
+               gl_Position = (FUSEE_MVP)* vec4(fuVertex, 1.0);
+ 
+            }";
+
+        private const string PsDrawDepth = @"
+            #ifdef GL_ES
+                precision mediump float;
+            #endif
+            void main(){
+               
+
+                   gl_FragColor= vec4(gl_FragCoord.z,gl_FragCoord.z,gl_FragCoord.z,1) ;
+
+           
+            }";
+
         #endregion
-        private Mesh _meshCube;
+        private Mesh _meshCube, _meshTeapot;
         private float3 _cubePos = float3.Zero;
+        // angle variables
+        private static float _angleHorz, _angleVert, _angleVelHorz, _angleVelVert;
+        private const float RotationSpeed = 1f;
+        private const float Damping = 0.92f;
 
         // variables for depth shader
         private ShaderProgram _spDepth;
@@ -80,6 +128,9 @@ namespace Examples.DepthVideo
         private IShaderParam _textureDepthParam;
         private ITexture _iTextureColor;
         private ITexture _iTextureDepth;
+
+        // variables to dra depthtectuer of the Scene
+        private ShaderProgram _spDrawDepth;
 
         // variables for color shader
         private ShaderProgram _spColor;
@@ -99,22 +150,26 @@ namespace Examples.DepthVideo
             RC.ClearColor = new float4(0.1f, 0.1f, 0.5f, 1);
 
             //Set zNear and zFar (1, 10)
+            // Wodth : Hwight :-> 1280 : 720
             Resize();
 
             //init mesh
             _meshCube = MeshReader.LoadMesh(@"Assets/Cube.obj.model");
-           
+            _meshTeapot = MeshReader.LoadMesh(@"Assets/Teapot.obj.model");
+
 
             //init shader
             _spDepth = RC.CreateShader(VsDepth, PsDepth);
-          //  _textureColorParam = _spDepth.GetShaderParam("texture1");
+           // _textureColorParam = _spDepth.GetShaderParam("textureColor");
             _textureDepthParam = _spDepth.GetShaderParam("textureDepth");
+
+            _spDrawDepth = RC.CreateShader(VsDrawDepth, PsDrawDepth);
 
             _spColor = Shaders.GetDiffuseColorShader(RC);
             _colorParam = _spColor.GetShaderParam("color");
 
             //Load Videos
-            ImportVideos(_framesListColorVideo, "Assets/demoSmall.mkv", _framesListDepthVideo, "Assets/demoDepthSmall.mkv");
+            ImportVideos(_framesListColorVideo, "Assets/demoFarSmall.mkv", _framesListDepthVideo, "Assets/demoFarDepthSmall.mkv");
             _framesListColorEnumerator = _framesListColorVideo.GetEnumerator();
             _framesListDepthEnumerator = _framesListDepthVideo.GetEnumerator();
         }
@@ -129,35 +184,56 @@ namespace Examples.DepthVideo
                 CloseGameWindow();
             // move per keyboard
             if (Input.Instance.IsKey(KeyCodes.Left))
-                _cubePos.x++;
+                _cubePos.x+=0.5f;
 
             if (Input.Instance.IsKey(KeyCodes.Right))
-                _cubePos.x--;
+                _cubePos.x -= 0.5f;
 
             if (Input.Instance.IsKey(KeyCodes.Up))
-                _cubePos.z++;
+                _cubePos.z+= 0.5f;
 
             if (Input.Instance.IsKey(KeyCodes.Down))
-                _cubePos.z--;
+                _cubePos.z -= 0.5f;
 
+            // move per mouse
+            if (Input.Instance.IsButton(MouseButtons.Left))
+            {
+                _angleVelHorz = -RotationSpeed * Input.Instance.GetAxis(InputAxis.MouseX);
+                _angleVelVert = -RotationSpeed * Input.Instance.GetAxis(InputAxis.MouseY);
+            }
+            else
+            {
+                var curDamp = (float)Math.Exp(-Damping * Time.Instance.DeltaTime);
+
+                _angleVelHorz *= curDamp;
+                _angleVelVert *= curDamp;
+            }
+
+            _angleHorz += _angleVelHorz;
+            _angleVert += _angleVelVert;
+
+
+
+            var mtxRot = float4x4.CreateRotationX(_angleVert) * float4x4.CreateRotationY(_angleHorz);
 
 
             _currentVideoFrames = GetCurrentVideoForames();
             CrateTextures(_currentVideoFrames);
 
+     
             var mtxCam = float4x4.LookAt(0, 0, -10, 0, 0, 0, 0, 1, 0);
+
             
-            RC.SetShader(_spDepth);
-            RC.ModelView = mtxCam * float4x4.Scale(0.01f) * float4x4.CreateTranslation(0,1, -7);
-            //var fm = RC.ModelView;
-           // Console.WriteLine(fm);
-            //  RC.SetShaderParamTexture(_textureColorParam, _iTextureColor);
-            RC.SetShaderParamTexture(_textureDepthParam, _iTextureDepth);
+            
+            RC.SetShader(_spDrawDepth);
+            RC.ModelView = mtxCam * mtxRot * float4x4.Scale(0.01f) * float4x4.CreateTranslation(_cubePos.x, 0, _cubePos.z);
+            //RC.SetShaderParam(_colorParam, new float4(1,0,0,1));
             RC.Render(_meshCube);
 
-            RC.SetShader(_spColor);
-            RC.ModelView = mtxCam * float4x4.Scale(0.01f) * float4x4.CreateTranslation(_cubePos.x, 0, _cubePos.z);
-            RC.SetShaderParam(_colorParam, new float4(1,0,0,1));
+            RC.SetShader(_spDepth);
+            RC.ModelView = mtxCam * float4x4.Scale(0.01f) * float4x4.CreateTranslation(0, 1, -2);
+            // RC.SetShaderParamTexture(_textureColorParam, _iTextureColor);
+            RC.SetShaderParamTexture(_textureDepthParam, _iTextureDepth);
             RC.Render(_meshCube);
 
             Present();
@@ -260,9 +336,11 @@ namespace Examples.DepthVideo
         // is called when the window was resized
         public override void Resize()
         {
+            
             RC.Viewport(0, 0, Width, Height);
 
             var aspectRatio = Width / (float)Height;
+
             RC.Projection = float4x4.CreatePerspectiveFieldOfView(MathHelper.PiOver4, aspectRatio, 1, 10);
         }
 
